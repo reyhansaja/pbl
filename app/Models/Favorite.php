daa9fb19253b5f805c6772427f34ccff1dda1196
vanesa <vanesshh@MacBook-Air-vanesa.local>
2026-05-07 08:40:48 +0700

    tambah model favorit, review, reply

---
+++ app/Models/Favorite.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Favorite extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'cafe_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function cafe()
+    {
+        return $this->belongsTo(Cafe::class);
+    }
+}
